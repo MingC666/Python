cd6cb911e207f02194dea9f693ba74f140b6a6c9
MingC <chenming0317@gmail.com>
2022-08-21 01:04:35 -0700

    update 08/21/2022

---
+++ z_for_else.py
@@ -0,0 +1,10 @@
+for x in range(0, 101, 5):
+	print(x)
+	if x > 50: break
+else:
+	print("End 5 inrement process")
+# the 'else' block will not execute
+
+for x in range(10):
+	pass
+# for  loop cannot be empty, but can use pass
